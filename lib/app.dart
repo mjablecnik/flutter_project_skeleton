742265c0bba41cb2079f3096d83a80aafe822d16
Martin Jablečník <martin.jablecnik@email.cz>
2024-03-12 21:59:13 +0100

    Update TextScaler

--- lib/app.dart
+++ lib/app.dart
@@ -45,7 +45,7 @@ class MyApp extends StatelessWidget {
               },
               builder: (context, child) {
                 return MediaQuery(
-                  data: MediaQuery.of(context).copyWith(textScaleFactor: 1.25),
+                  data: MediaQuery.of(context).copyWith(textScaler: const TextScaler.linear(1.25)),
                   child: child!,
                 );
               },
