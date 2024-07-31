266110af72e08a3247e99c2214c1f0b07b137200
Martin Jablečník <martin.jablecnik@email.cz>
2024-07-31 17:47:51 +0200

    Update test devices

--- test/storybook_test.dart
+++ test/storybook_test.dart
@@ -4,10 +4,16 @@ import 'package:storybook_toolkit_tester/storybook_toolkit_tester.dart';
 import '../storybook/app.dart';
 
 void main() => testStorybook(
-  storybook(),
-  devices: {Devices.android.smallPhone, Devices.ios.iPhone12, Devices.ios.iPad},
-  filterStories: (Story story) {
-    final skipStories = [];
-    return !skipStories.contains(story.name);
-  },
-);
+      storybook(),
+      devices: {
+        Devices.ios.iPhoneSE,
+        Devices.ios.iPhone13ProMax,
+        Devices.android.onePlus8Pro,
+        Devices.android.samsungGalaxyA50,
+        Devices.android.sonyXperia1II,
+      },
+      filterStories: (Story story) {
+        final skipStories = [];
+        return !skipStories.contains(story.name);
+      },
+    );
